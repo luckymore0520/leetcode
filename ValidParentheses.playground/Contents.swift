42b4db282853805e76ea341f7d2f46a1605eee24
luckymore <njuluckwang@gmail.com>
2017-02-27 12:48:49 +0800

    valid parentheses

---
+++ ValidParentheses.playground/Contents.swift
@@ -0,0 +1,54 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Stack<T>{
+    var stack: [T]
+    init(){
+        stack = [T]()
+    }
+    func push(element:T){
+        stack.append(element)
+    }
+    func pop() -> T?{
+        if (stack.isEmpty) {
+            return nil;
+        }
+        return stack.removeLast()
+    }
+    func isEmpty() -> Bool {
+        return stack.isEmpty;
+    }
+    func peek() -> T? {
+        return stack.last
+    }
+    func size() -> Int {
+        return stack.count;
+    }
+}
+
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        let left = ["(".characters.first!,"[".characters.first!,"{".characters.first!]
+        let right = [")".characters.first!,"]".characters.first!,"}".characters.first!]
+        let stack = Stack<Character>()
+        for character in s.characters {
+            if left.contains(character) {
+                stack.push(element: character)
+            } else if let rightIndex = right.index(of: character) {
+                let leftPair = left[rightIndex]
+                let top = stack.pop();
+                if (leftPair != top) {
+                    return false
+                }
+            } else {
+                return false
+            }
+            
+        }
+        return stack.isEmpty()
+    }
+}
+
+let solution = Solution()
+solution.isValid("[]()([])")
